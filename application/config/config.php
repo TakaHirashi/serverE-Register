41f2e452cb650dd17848fe676cb05223990ddcaf
TakaHirashi <rakhacrazy7@gmail.com>
2018-03-26 16:31:59 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost/serverE-Register/';
+$config['base_url'] = 'https://github.com/TakaHirashi/serverE-Register/';
 
 /*
 |--------------------------------------------------------------------------
